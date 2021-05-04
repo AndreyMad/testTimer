125f949c502c8667ad493e56c1045d291542955a
AndreyMad <andrey.mad.87@gmail.com>
2021-05-04 23:28:04 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,57 +1,72 @@
-import React, { Component } from "react";
-// import "./style.css";
-import { interval, timer, Subject, NEVER } from "rxjs";
-import { takeUntil, switchMap, tap, pipe } from "rxjs/operators";
+import React, { Component, useCallback } from "react";
+import { render } from "react-dom";
+import { useEffect, useState } from "react";
+import { interval, Subject } from "rxjs";
+import { takeUntil, take, tap } from "rxjs/operators";
+import style from "./App.module.css";
 
-class App extends Component {
-  state = {
-    count: true,
-    time: 0,
-  };
+export default function App() {
+  const [sec, setSec] = useState(0);
+  const [status, setStatus] = useState("stop");
+  const [waitTimeStamp, setWaitTimeStamp] = useState(0);
 
-  startTimer() {
-    
-switchMap((state) =>
-      state.count
-        ? interval(1000, 1000).pipe(
-            tap((val) => {
-              this.setState({ time: val });
-            })
-          )
-        : NEVER
-    ).subscribe()
-   
-    // return () => {
-    //     unsubscribe$.next();
-    //     // unsubscribe$.complete();
-    //   };
-  }
+  useEffect(() => {
+    const unsubscribe$ = new Subject();
+    interval(1000)
+      .pipe(takeUntil(unsubscribe$))
+      .subscribe(() => {
+        if (status === "start") {
+          setSec((val) => val + 1000);
+        }
+      });
 
-  start = () => this.setState({ count: "run" }, this.startTimer);
-  stop = () => this.setState({ count: "stop", time: 0 });
-  reset = () => this.setState({ time: 0 });
+    return () => {
+      unsubscribe$.next();
+      unsubscribe$.complete();
+    };
+  }, [status]);
 
-  render() {
-    const { time } = this.state;
-    // const source = interval(1000);
-    //output: 0,1,2,3,4,5....
-    // const subscribe = source.subscribe(val => console.log(val));
-    return (
-      <>
-        <div>
-          <span> {new Date(time).toISOString().slice(11, 19)}</span>
-          <button className="start-button" onClick={this.start}>
-            Start
-          </button>
-          <button className="stop-button" onClick={this.stop}>
-            Stop
-          </button>
-          <button onClick={this.reset}>Reset</button>
-          {/* <button onClick={wait}>Wait</button> */}
-        </div>
-      </>
-    );
-  }
+  const start = useCallback(() => {
+    setStatus("start");
+  }, []);
+
+  const stop = useCallback(() => {
+    setStatus("stop");
+    setSec(0);
+  }, []);
+
+  const reset = useCallback(() => {
+    setSec(0);
+  }, []);
+
+  const wait = useCallback(() => {
+    const timeStamp = new Date().getTime();
+    if (timeStamp - waitTimeStamp < 300) {
+      setStatus("wait");
+      return;
+    }
+    setWaitTimeStamp(timeStamp);
+  }, [waitTimeStamp]);
+
+  return (
+    <div className={style.container}>
+      <span className={style.time}>
+        {new Date(sec).toISOString().slice(11, 19)}
+      </span>
+      <button className="startBtn" onClick={start}>
+        {status === "wait" ? "Resume" : "Start"}
+      </button>
+      <button
+        disabled={status === "stop" || false}
+        className="stopBtn"
+        onClick={stop}
+      >
+        Stop
+      </button>
+      <button onClick={reset}>Reset</button>
+      <button onClick={wait}>Wait</button>
+    </div>
+  );
 }
 
-export default App;
+render(<App />, document.getElementById("root"));
